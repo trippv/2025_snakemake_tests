682f50c92f4347d3230b597fa6e797df6c552efc
trippv <miguel.tripp@gmail.com>
2025-06-16 12:19:14 -0600

    cambios en el script de slurm

--- scripts/run_snakemake_slurm.sh
+++ scripts/run_snakemake_slurm.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=RnaseqSnake
-#SBATCH --output=miworkflow_%j.log
-#SBATCH --error=miworkflow_%j.err
+#SBATCH --output=rnaseq_snake_%j.log
+#SBATCH --error=rnaseq_snake_%j.err
 #SBATCH -p cicese
 #SBATCH --ntasks-per-node=24
 #SBATCH --mem=100GB
@@ -11,13 +11,12 @@
 source /LUSTRE/apps/Miniforge/2024/miniforge3/etc/profile.d/conda.sh
 source /LUSTRE/apps/Miniforge/2024/miniforge3/etc/profile.d/mamba.sh
 CONDA_BASE=$(conda info --base)
-source $CONDA_BASE/etc/profile.d/conda.sh
+
 conda activate miworkflow_env  # cambia al entorno que uses
 
 cd $SLURM_SUBMIT_DIR
 
-snakemake -R $(snakemake --list-params-changes) \
- -d ./ \
+snakemake -d ./ \
  --snakefile snakefile \
  --workflow-profile ./profiles/slurm \
  --cores 24 \
